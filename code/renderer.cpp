40c7ba32d102015f1e4ebd847a6ccfb9a85e6168
Guilherme <guilhermegsilverio@gmail.com>
2018-01-02 17:38:58 -0200

    code 1.1

--- code/renderer.cpp
+++ code/renderer.cpp
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <windows.h>
-#define comprimento 32
-#define altura 24
+#define comprimento 30
+#define altura 10
 
 int main ()
 {
 
-	char bitmap [altura][comprimento];
+	char bitmap [altura*comprimento];
 	
 	for(int i=0;i<altura;i++)
 	{
 		for(int j=0;j<comprimento;j++)
 		{	
-			bitmap[i][j] = -80;
+			bitmap[i*j] = -80;
 		}
 
 	}
@@ -21,7 +21,7 @@ int main ()
 	{
 		for(int j=0; j<comprimento; j++)
 		{
-			printf("%c", bitmap[i][j]);
+			printf("%c", bitmap[i*j]);
 		}
 		printf("\n");
 	}
