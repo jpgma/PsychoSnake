4624ce9e27efb9a0944f2a47d3bc92e51e08c8df
Guilherme <guilhermegsilverio@gmail.com>
2018-01-02 17:19:19 -0200

    code 1

--- code/renderer.cpp
+++ code/renderer.cpp
@@ -1,7 +1,32 @@
-
 #include <stdio.h>
+#include <windows.h>
+#define comprimento 32
+#define altura 24
 
-void main()
+int main ()
 {
-	printf("OKOKOK\n");
+
+	char bitmap [altura][comprimento];
+	
+	for(int i=0;i<altura;i++)
+	{
+		for(int j=0;j<comprimento;j++)
+		{	
+			bitmap[i][j] = -80;
+		}
+
+	}
+	
+	for(int i=0;i<altura; i++)
+	{
+		for(int j=0; j<comprimento; j++)
+		{
+			printf("%c", bitmap[i][j]);
+		}
+		printf("\n");
+	}
+
+
+
+	return 0;
 }
