58ba99a78359afc2e89e31a2ee814a7b26005cec
Guilherme <guilhermegsilverio@gmail.com>
2018-01-03 18:16:54 -0200

    corzinha

--- code/renderer.cpp
+++ code/renderer.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <windows.h>
-
-#define comprimento 20
-#define altura 10
+#define WIN32_LEAN_AND_MEAN
+#define SCREEN_WIDTH 20
+#define SCREEN_HEIGHT 10
 
 static char LOADED_BITMAP[] = 
 {
@@ -20,12 +20,13 @@ static char LOADED_BITMAP[] =
 
 void LimparTela()
 {
-	char bitmap [altura*comprimento];
-	for(int y = 1; y <= altura; y++)
+	char bitmap [SCREEN_HEIGHT*SCREEN_WIDTH];
+	for(int y = 1; y <= SCREEN_HEIGHT; y++)
 	{
-		for(int x = 1; x <= comprimento; x++)
+		for(int x = 1; x <= SCREEN_WIDTH; x++)
 		{	
-			bitmap[x+(y*altura)] = -80;
+
+			bitmap[x+(y*SCREEN_HEIGHT)] = -80;
 		}
 	}
 }
@@ -33,12 +34,27 @@ void LimparTela()
 
 void PrintarBitMap()
 {
-	char bitmap [altura*comprimento];
-	for(int y = 1; y <= altura; y++)
+	char bitmap [SCREEN_HEIGHT*SCREEN_WIDTH];
+	for(int y = 1; y <= SCREEN_HEIGHT; y++)
 	{
-		for(int x = 1; x <= comprimento; x++)
+		for(int x = 1; x <= SCREEN_WIDTH; x++)
 		{
-			printf("%c", bitmap[x+(y*altura)]);
+			HANDLE hOutput = (HANDLE)GetStdHandle( STD_OUTPUT_HANDLE );
+
+			COORD dwBufferSize = { SCREEN_WIDTH,SCREEN_HEIGHT };
+			COORD dwBufferCoord = { 0, 0 };
+			SMALL_RECT rcRegion = { 0, 0, SCREEN_WIDTH-1, SCREEN_HEIGHT-1 };
+
+			CHAR_INFO buffer[SCREEN_HEIGHT][SCREEN_WIDTH];
+
+			ReadConsoleOutput( hOutput, (CHAR_INFO *)buffer, dwBufferSize,
+			dwBufferCoord, &rcRegion );
+
+			buffer[y][x].Attributes = 0x0E;
+
+			WriteConsoleOutput( hOutput, (CHAR_INFO *)buffer, dwBufferSize,
+			dwBufferCoord, &rcRegion );
+			printf("%c", bitmap[x+(y*SCREEN_HEIGHT)]);
 		}
 		printf("\n");
 	}
