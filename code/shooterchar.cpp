32c2b09ce7c894f10be456176d0feb5b51b6758c
JP Morais <jpgmorais3@gmail.com>
2018-09-20 20:40:11 -0300

    Movimentando com V2

--- code/shooterchar.cpp
+++ code/shooterchar.cpp
@@ -6,23 +6,6 @@
 #define TARGET_FPS 60
 #define TARGET_MS_PER_FRAME (1000.0/(r64)TARGET_FPS)
 
-struct GameState
-{
-    b32 initialized;
-    r32 vx;
-    r32 vy;
-
-    r32 map_px,map_py;
-
-    //POSIÇÕES NO ESPAÇO DO MUNDO
-    r32 px;
-    r32 py;
-
-    //Qtd de chars que vai andar em 1s
-    r32 vel_units_per_sec;
-};
-
-
 struct V2
 {
     r32 x,y;
@@ -38,15 +21,32 @@ V2 operator * (V2 v, r32 s)
     return{v.x*s, v.y*s};
 }
 
+struct GameState
+{
+    b32 initialized;
+
+    //POSIÇÃO NO ESPAÇO DO MUNDO
+    V2 p;
+
+    //VELOCIDADE NO ESPAÇO DO MUNDO
+    V2 v;
+
+    //Qtd de chars que vai andar em 1s
+    r32 vel_units_per_sec;
+};
 
 internal void 
 GameUpdateAndRender (GameState *game_state, Renderer *renderer, r32 dt)
 {
 	if(!game_state->initialized)
 	{
-		game_state->initialized=true;
-		game_state->vel_units_per_sec=10.0f;
+        
+        game_state->p = {0.0f,0.0f};
+        game_state->v = {0.0f,0.0f};
+        game_state->vel_units_per_sec = 10.0f;
 
+
+		game_state->initialized=true;
 	}
 		
     ClearRenderBuffer(&renderer->buffer, ' ', COLOR(165,165,249,255),COLOR(165,165,249,255));
@@ -83,86 +83,34 @@ GameUpdateAndRender (GameState *game_state, Renderer *renderer, r32 dt)
 	bool right = IS_KEY_DOWN(VK_RIGHT);
 
 
-	game_state->vx = (right ? 1.0 : 0.0)+(left ? -1.0 : 0.0);
-	game_state->vy = (down ? 1.0 : 0.0)+(up ? -1.0 : 0.0);
-	/*if(right)
-	{
-		//ORIENTAÇÃO DA DIREÇÃO QUE O MOVIMENTO VAI
-		game_state->vx=1.0;
-		game_state->vy=0.0;
-		
-	    game_state->posicao_x++;
-	    if(game_state->posicao_x>(SCREEN_WIDTH-1))
-	    {
-	    	game_state->posicao_x=0.0;
-	    }	}
-	if(left)
-	{
-		//ORIENTAÇÃO DA DIREÇÃO QUE O MOVIMENTO VAI
-		game_state->vx=-1.0;
-		game_state->vy=0.0;
+	//DESLOCAMENTO DO FRAME
+    game_state->v.x = (right ? 1.0 : 0.0)+(left ? -1.0 : 0.0);
+    game_state->v.y = (down ? 1.0 : 0.0)+(up ? -1.0 : 0.0);
+	game_state->v = game_state->v * (game_state->vel_units_per_sec*dt);
 
-		
-	    game_state->posicao_x--;
-	    if(game_state->posicao_x<(0.0))
-	    {
-	    	game_state->posicao_x=(SCREEN_WIDTH-1);
-	    }
-	}
-	if(up)
-	{
-		//ORIENTAÇÃO DA DIREÇÃO QUE O MOVIMENTO VAI
-		game_state->vx=0.0;
-		game_state->vy=-1.0;
-
-	    /*game_state->posicao_y--;
-	    if(game_state->posicao_y<0.0)
-	    {
-	    	game_state->posicao_y=(SCREEN_HEIGHT-1);
-	    }
-	}
-	if(down)
-	{
-		//ORIENTAÇÃO DA DIREÇÃO QUE O MOVIMENTO VAI
-		game_state->vx=0.0;
-		game_state->vy=1.0;
-
-	    /*game_state->posicao_y++;
-	    if(game_state->posicao_y>(SCREEN_HEIGHT-1))
-	    {
-	    	game_state->posicao_y=0.0;
-	    }
-	}*/
-
-	//DESLOCAMENTO DO FRAME EM VX E VY
-	game_state->vx*= (game_state->vel_units_per_sec*dt);
-	game_state->vy*= (game_state->vel_units_per_sec*dt);
+    //BARRAMENTO DA JANELA
+    if(game_state->p.x > SCREEN_WIDTH-1)
+    {
+        game_state->p.x = SCREEN_WIDTH - 1;
+    }
+    else if(game_state->p.x < 0.0f)
+    {
+        game_state->p.x = 0.0f;
+    }
+    if(game_state->p.y > SCREEN_HEIGHT-1)
+    {
+        game_state->p.y = SCREEN_HEIGHT - 1;
+    }
+    else if(game_state->p.y < 0.0f)
+    {
+        game_state->p.y = 0.0f;
+    }
 
 	//POSIÇÃO PERSONAGEM NO MUNDO
-	game_state->px+=game_state->vx;
-	game_state->py+=game_state->vy;
-	
+	game_state->p = game_state->p + game_state->v;
 
-	//BARRAMENTO DA JANELA
-	if(game_state->px>SCREEN_WIDTH-1)
-	{
-		game_state->px= SCREEN_WIDTH-1;
-	}
-	else if(game_state->px<0.0)
-	{
-		game_state->px=0.0;
-	}
-	if(game_state->py>SCREEN_HEIGHT-1)
-	{
-		game_state->py = SCREEN_HEIGHT-1;
-	}
-	else if(game_state->py<0.0)
-	{
-		game_state->py = 0.0;
-	}
-
-	ptx = (u16)game_state->px;
-	pty = (u16)game_state->py;
+	ptx = (u16)game_state->p.x;
+	pty = (u16)game_state->p.y;
 
 	SetChar(&renderer->buffer,ptx,pty,0xE000, COLOR(255,0,0,0), COLOR(0,0,0,0));
 }
