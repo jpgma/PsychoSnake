8270a60b5d4d581a1fbdae69af09ad1db463e8ea
MrJpMor <jpgmorais3@gmail.com>
2018-11-07 11:03:51 -0200

    Introduzindo V2, operaçoes basicas

--- code/spaceshooter.cpp
+++ code/spaceshooter.cpp
@@ -22,18 +22,56 @@
 //     ENEMY_STATE_DYING,
 // };
 
+struct V2
+{
+    r32 x,y;
+};
+#define V2(x,y) v2((x),(y))
+V2 v2(r32 x, r32 y) { return{x,y}; }
+V2 operator * (V2 v, r32 s) { return{v.x*s, v.y*s}; }
+V2 operator * (r32 s, V2 v) { return{v.x*s, v.y*s}; }
+V2 operator / (V2 v, r32 s) { return{v.x/s, v.y/s}; }
+V2 operator / (r32 s, V2 v) { return{v.x/s, v.y/s}; }
+V2 operator + (V2 a, V2 b)  { return{a.x+b.x, a.y+b.y};}
+V2 operator - (V2 a, V2 b)  { return{a.x-b.x, a.y-b.y};}
+
+inline r32
+Magnitude (V2 v)
+{
+    r32 res = sqrtf(v.x*v.x + v.y*v.y);
+    return res;
+}
+
+inline void
+Normalize (V2 *v)
+{
+    r32 mag2 = v->x*v->x + v->y*v->y;
+    if(mag2 > 0.0f)
+    {
+        r32 one_over_mag = 1.0f/sqrtf(mag2);
+        v->x *= one_over_mag;
+        v->y *= one_over_mag;
+    }
+}
+
+inline V2 
+Normalized (V2 v)
+{
+    V2 res = v;
+    Normalize(&res);
+    return res;
+}
+
 struct GameState
 {
     b32 initialized;
 
-    r32 player_px,player_py;
-    r32 player_dx,player_dy;
+    V2 player_p;
+    V2 player_d;
 
     u32 player_shot_index;
-    r32 player_shot_px[PLAYER_MAX_SHOTS];
-    r32 player_shot_py[PLAYER_MAX_SHOTS];
-    r32 player_shot_dx[PLAYER_MAX_SHOTS];
-    r32 player_shot_dy[PLAYER_MAX_SHOTS];
+    V2 player_shot_p[PLAYER_MAX_SHOTS];
+    V2 player_shot_d[PLAYER_MAX_SHOTS];
     r32 player_shot_cooldown;
 
     // r32 enemy_px[MAX_ENEMIES];
@@ -42,7 +80,7 @@ struct GameState
 };
 
 internal void
-PlayerShoot (GameState *game_state, r32 px, r32 py, r32 dx, r32 dy)
+PlayerShoot (GameState *game_state, V2 p, V2 d)
 {
     if(game_state->player_shot_cooldown >= PLAYER_SHOT_COOL_SECONDS)
     {
@@ -56,17 +94,14 @@ PlayerShoot (GameState *game_state, r32 px, r32 py, r32 dx, r32 dy)
         }
         u32 index = game_state->player_shot_index;
 
-        if(dx==0.0f && dy==0.0f)
+        if(d.x==0.0f && d.y==0.0f)
         {
-            dy = -1.0f;
+            d.y = -1.0f;
         }
 
-        game_state->player_shot_px[index] = px + ((PLAYER_WIDTH+1)*dx/2.0f);
-        game_state->player_shot_py[index] = py + ((PLAYER_HEIGHT+1)*dy/2.0f);
-
-        game_state->player_shot_dx[index] = dx;
-        game_state->player_shot_dy[index] = dy;
-
+        game_state->player_shot_p[index] = p + V2(((PLAYER_WIDTH+1)*d.x/2.0f),((PLAYER_HEIGHT+1)*d.y/2.0f));
+        game_state->player_shot_d[index] = d;
+        
         game_state->player_shot_cooldown = 0.0f;
     }
 }
@@ -74,10 +109,8 @@ PlayerShoot (GameState *game_state, r32 px, r32 py, r32 dx, r32 dy)
 internal void
 PlayerRemoveShot (GameState *game_state, u32 index)
 {
-    game_state->player_shot_px[index] = INFINITY;
-    game_state->player_shot_py[index] = INFINITY;
-    game_state->player_shot_dx[index] = 0.0f;
-    game_state->player_shot_dy[index] = 0.0f;
+    game_state->player_shot_p[index] = {INFINITY,INFINITY};
+    game_state->player_shot_d[index] = {0.0f,0.0f};
 }
 
 internal void
@@ -89,16 +122,16 @@ UpdateAndRenderShots (GameState *game_state, Renderer *renderer, r32 dt)
     Color player_shot_color = COLOR(255,100,100,255);
     for (s32 i = 0; i < PLAYER_MAX_SHOTS; ++i)
     {
-        game_state->player_shot_px[i] += game_state->player_shot_dx[i] * (player_shot_speed*dt);
-        game_state->player_shot_py[i] += game_state->player_shot_dy[i] * (player_shot_speed*dt);
 
-        SetChar(renderer, (u16)game_state->player_shot_px[i], (u16)game_state->player_shot_py[i], 
+        game_state->player_shot_p[i] = game_state->player_shot_p[i] + (game_state->player_shot_d[i] * (player_shot_speed*dt));
+
+        SetChar(renderer, (u16)game_state->player_shot_p[i].x, (u16)game_state->player_shot_p[i].y, 
                 GetGlyphIndex(renderer->font,' '), player_shot_color,player_shot_color);
     }
 }
 
 internal void
-DrawShip (Renderer *renderer, s32 screen_x, s32 screen_y, r32 dx, r32 dy, u32 width, u32 height, Color color)
+DrawShip (Renderer *renderer, s32 screen_x, s32 screen_y, V2 d, u32 width, u32 height, Color color)
 {
     s32 half_width = width*0.5f;
     s32 half_height = height*0.5f;
@@ -111,8 +144,8 @@ DrawShip (Renderer *renderer, s32 screen_x, s32 screen_y, r32 dx, r32 dy, u32 wi
     if((fabs((SCREEN_WIDTH/2.0f) - (r32)screen_x) <= (SCREEN_WIDTH/2.0f + half_width)) &&
        (fabs((SCREEN_HEIGHT/2.0f) - (r32)screen_y) <= (SCREEN_HEIGHT/2.0f + half_height)))
     {
-        s32 dir_x = (screen_x + (dx*half_width));
-        s32 dir_y = (screen_y + (dy*half_height));
+        s32 dir_x = (screen_x + (d.x*half_width));
+        s32 dir_y = (screen_y + (d.y*half_height));
 
         for (s32 y = miny; y <= maxy; ++y)
         {
@@ -137,10 +170,8 @@ GameUpdateAndRender (GameState *game_state, Renderer *renderer, r32 dt)
 {
     if(!game_state->initialized)
     {
-        game_state->player_px = SCREEN_WIDTH/2;
-        game_state->player_py = SCREEN_HEIGHT/2;
-        game_state->player_dx = 0.0f;
-        game_state->player_dy = -1.0f;
+        game_state->player_p = V2((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2));
+        game_state->player_d = V2(0.0f,-1.0f);
         for (u32 i = 0; i < PLAYER_MAX_SHOTS; ++i)
         {
             PlayerRemoveShot(game_state, i);
@@ -159,24 +190,23 @@ GameUpdateAndRender (GameState *game_state, Renderer *renderer, r32 dt)
 
     r32 player_speed = 15.0f;
 
-    game_state->player_dx = (left ? -1.0f : (right ? 1.0f : 0.0f));
-    game_state->player_dy = (up   ? -1.0f : (down  ? 1.0f : 0.0f));
+    game_state->player_d = V2((left ? -1.0f : (right ? 1.0f : 0.0f)),
+                              (up   ? -1.0f : (down  ? 1.0f : 0.0f)));
+    Normalize(&game_state->player_d);
 
-    game_state->player_px += game_state->player_dx * (player_speed * dt);
-    game_state->player_py += game_state->player_dy * (player_speed * dt);
+    game_state->player_p = game_state->player_p + (game_state->player_d * (player_speed * dt));
 
     if(space)
     {
         PlayerShoot(game_state, 
-                    game_state->player_px,game_state->player_py,
-                    game_state->player_dx,game_state->player_dy);
+                    game_state->player_p,
+                    game_state->player_d);
     }
 
     Color red = COLOR(255,0,0,255);
     DrawShip(renderer, 
-             (s32)game_state->player_px, (s32)game_state->player_py,
-             game_state->player_dx, game_state->player_dy,
-             PLAYER_WIDTH, PLAYER_HEIGHT, red);
+             (s32)game_state->player_p.x, (s32)game_state->player_p.y,
+             game_state->player_d, PLAYER_WIDTH, PLAYER_HEIGHT, red);
 
     UpdateAndRenderShots(game_state, renderer, dt);
 }
