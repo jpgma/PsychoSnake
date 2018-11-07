5631d7b54c2a512853217a40022a2b5e1885ff25
MrJpMor <jpgmorais3@gmail.com>
2018-11-06 22:44:46 -0200

    direcao do movimento

--- code/spaceshooter.cpp
+++ code/spaceshooter.cpp
@@ -7,18 +7,34 @@
 #define TARGET_FPS 60
 #define TARGET_MS_PER_FRAME (1000.0/(r64)TARGET_FPS)
 
-#define PLAYER_WIDTH 6
-#define PLAYER_HEIGHT 6
+#define PLAYER_WIDTH 3
+#define PLAYER_HEIGHT 3
+
+// #define MAX_ENEMIES 3
+// #define ENEMY_WIDTH 3
+// #define ENEMY_HEIGHT 5
+
+// enum EnemyState
+// {
+//     ENEMY_STATE_DEAD,
+//     ENEMY_STATE_ALIVE,
+//     ENEMY_STATE_DYING,
+// };
 
 struct GameState
 {
     b32 initialized;
 
     r32 player_px,player_py;
+    r32 player_dx,player_dy;
+
+    // r32 enemy_px[MAX_ENEMIES];
+    // r32 enemy_py[MAX_ENEMIES];
+    // u32 enemy_state[MAX_ENEMIES];
 };
 
 internal void
-DrawShip (Renderer *renderer, s32 screen_x, s32 screen_y, u32 width, u32 height, Color color)
+DrawShip (Renderer *renderer, s32 screen_x, s32 screen_y, r32 dx, r32 dy, u32 width, u32 height, Color color)
 {
     s32 half_width = width*0.5f;
     s32 half_height = height*0.5f;
@@ -35,12 +51,55 @@ DrawShip (Renderer *renderer, s32 screen_x, s32 screen_y, u32 width, u32 height,
         {
             for (s32 x = minx; x <= maxx; ++x)
             {
-                if(x == screen_x && y == screen_y)
+                Color c = color;
+                if(dx > 0.0f)
                 {
-                    SetChar(renderer, (u16)x, (u16)y, GetGlyphIndex(renderer->font,' '), COLOR_WHITE,COLOR_WHITE);
+                    if(dy > 0.0f)
+                    { // direita, baixo
+                        if((x==maxx) && (y==maxy)) c = COLOR_WHITE;
+                    }
+                    else if(dy < 0.0f)
+                    { // direita, cima
+                        if((x==maxx) && (y==miny)) c = COLOR_WHITE;
+                    }
+                    else
+                    { // direita
+                        if(x == maxx) c = COLOR_WHITE;
+                    }
+                }
+                else if(dx < 0.0f)
+                {
+
+                    if(dy > 0.0f)
+                    { // esquerda, baixo
+                        if((x==minx) && (y==maxy)) c = COLOR_WHITE;
+                    }
+                    else if(dy < 0.0f)
+                    { // esquerda, cima
+                        if((x==minx) && (y==miny)) c = COLOR_WHITE;
+                    }
+                    else
+                    { // esquerda
+                        if(x == minx) c = COLOR_WHITE;
+                    }
                 }
                 else
-                    SetChar(renderer, (u16)x, (u16)y, GetGlyphIndex(renderer->font,' '), color,color);
+                {
+                    if(dy > 0.0f)
+                    { // baixo
+                        if(y == maxy) c = COLOR_WHITE;
+                    }
+                    else if(dy < 0.0f)
+                    { // cima
+                        if(y == miny) c = COLOR_WHITE;
+                    }
+                    else
+                    { // parado
+                        if((x==screen_x) && (y==screen_y)) c = COLOR_WHITE;
+                    }
+                }
+                
+                SetChar(renderer, (u16)x, (u16)y, GetGlyphIndex(renderer->font,' '), c,c);
             }
         }
     }
@@ -55,6 +114,8 @@ GameUpdateAndRender (GameState *game_state, Renderer *renderer, r32 dt)
     {
         game_state->player_px = SCREEN_WIDTH/2;
         game_state->player_py = SCREEN_HEIGHT/2;
+        game_state->player_dx = 0.0f;
+        game_state->player_dy = -1.0f;
         game_state->initialized = true;
     }
 
@@ -66,13 +127,17 @@ GameUpdateAndRender (GameState *game_state, Renderer *renderer, r32 dt)
     bool right = IS_KEY_DOWN(VK_RIGHT);
 
     r32 player_speed = 15.0f;
-    r32 player_speed_x = (left ? -1.0f : (right ? 1.0f : 0.0f)) * (player_speed * dt);
-    r32 player_speed_y = (up   ? -1.0f : (down  ? 1.0f : 0.0f)) * (player_speed * dt);
 
-    game_state->player_px += player_speed_x;
-    game_state->player_py += player_speed_y;
+    game_state->player_dx = (left ? -1.0f : (right ? 1.0f : 0.0f));
+    game_state->player_dy = (up   ? -1.0f : (down  ? 1.0f : 0.0f));
+
+    game_state->player_px += game_state->player_dx * (player_speed * dt);
+    game_state->player_py += game_state->player_dy * (player_speed * dt);
 
     Color red = COLOR(255,0,0,255);
-    DrawShip(renderer, (s32)game_state->player_px, (s32)game_state->player_py, PLAYER_WIDTH, PLAYER_HEIGHT, red);
+    DrawShip(renderer, 
+             (s32)game_state->player_px, (s32)game_state->player_py,
+             game_state->player_dx, game_state->player_dy,
+             PLAYER_WIDTH, PLAYER_HEIGHT, red);
 }
 
